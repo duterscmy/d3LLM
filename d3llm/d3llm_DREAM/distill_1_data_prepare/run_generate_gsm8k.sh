8a5343f95028df153cf563f7714d497e27151def
mingyu cao <m.cao@surrey.ac.uk>
2026-02-24 20:12:57 +0000

    fix: load gsm8k

--- d3llm/d3llm_DREAM/distill_1_data_prepare/run_generate_gsm8k.sh
+++ d3llm/d3llm_DREAM/distill_1_data_prepare/run_generate_gsm8k.sh
@@ -2,11 +2,10 @@
 
 start_idx=0
 end_idx=1000
-python d3llm_dream_generate_partly.py \
+python d3llm/d3llm_DREAM/distill_1_data_prepare/d3llm_dream_generate_partly.py \
         --start_idx $start_idx \
         --end_idx $end_idx \
         --steps 256 \
-        --gen_length 256 \
-        --block_length 32 \
+        --gen_length 256 --block_length 32 \
         --output_file trajectory_data.json \
         --max_data_num 10000
