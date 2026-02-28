6d98f76340b3b44412081be64affabefae542ab9
mingyu cao <m.cao@surrey.ac.uk>
2026-02-28 22:17:07 +0000

    add

--- d3llm/d3llm_DREAM/distill_2_training/d3llm_dream_train_no_ds.py
+++ d3llm/d3llm_DREAM/distill_2_training/d3llm_dream_train_no_ds.py
@@ -205,13 +205,17 @@ def forward_process_with_trajectory(
         seg_len = mask_end - mask_start
         if seg_len > 0:
             if traj_step is not None:
+                print("traj_step is not None")
                 traj_tensor = torch.tensor(traj_step, device=device, dtype=torch.long)
                 seg_mask = (traj_tensor[mask_start:mask_end] == mask_token_id)
             else:
+                print("traj_step is None")
                 p_mask = (1 - eps) * mask_ratio + eps
                 seg_mask = torch.rand(seg_len, device=device) < p_mask
             
             # Apply mask
+            print(mask_start, mask_end)
+            print(seg_mask.size())
             masked_indices[i, mask_start:mask_end] = seg_mask
             if use_complementary_loss:
                 masked_indices_rev[i, mask_start:mask_end] = ~seg_mask
