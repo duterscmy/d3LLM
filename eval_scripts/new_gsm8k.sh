f70770371403bd4983de6d1647dc454092929825
mingyu cao <m.cao@surrey.ac.uk>
2026-03-01 21:19:46 +0000

    add

--- eval_scripts/new_gsm8k.sh
+++ eval_scripts/new_gsm8k.sh
@@ -17,7 +17,7 @@ cd /projects/u6er/public/mingyu/d3LLM/utils/utils_Dream/eval_instruct
 
 CUDA_VISIBLE_DEVICES=0 PYTHONPATH=. accelerate launch --main_process_port 12334 -m lm_eval \
     --model diffllm \
-    --model_args torch_compile=False,pretrained=Dream-org/Dream-v0-Instruct-7B,trust_remote_code=True,max_new_tokens=256,diffusion_steps=256,dtype="bfloat16",temperature=0.1,top_p=0.9,alg="entropy" \
+    --model_args torch_compile=False,pretrained=/lus/lfs1aip2/projects/public/u6er/mingyu/models/Dream-v0-Instruct-7B,trust_remote_code=True,max_new_tokens=256,diffusion_steps=256,dtype="bfloat16",temperature=0.1,top_p=0.9,alg="entropy" \
     --tasks gsm8k_cot_zeroshot \
     --device cuda \
     --batch_size 1 \
