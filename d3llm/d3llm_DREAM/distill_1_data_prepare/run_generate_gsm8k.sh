ca598723c85452991b526c791b34eef7f5d16cbe
mingyu cao <m.cao@surrey.ac.uk>
2026-03-08 15:57:31 +0000

    add

--- d3llm/d3llm_DREAM/distill_1_data_prepare/run_generate_gsm8k.sh
+++ d3llm/d3llm_DREAM/distill_1_data_prepare/run_generate_gsm8k.sh
@@ -5,7 +5,7 @@ end_idx=30
 python d3llm/d3llm_DREAM/distill_1_data_prepare/d3llm_dream_generate_partly.py \
         --start_idx $start_idx \
         --end_idx $end_idx \
-        --steps 256 \
-        --gen_length 256 --block_length 1 \
+        --steps 512 \
+        --gen_length 512 --block_length 1 \
         --output_file trajectory_data_ar.json \
         --max_data_num 100000
