8af1253f6b52217e0b291a6e5eaadeb3ad4c4b6a
mingyu cao <m.cao@surrey.ac.uk>
2026-03-08 16:30:24 +0000

    add

--- d3llm/d3llm_DREAM/distill_1_data_prepare/submit_slurm.sh
+++ d3llm/d3llm_DREAM/distill_1_data_prepare/submit_slurm.sh
@@ -22,15 +22,14 @@ for ((start_idx=$START; start_idx<$END; start_idx+=$STEP)); do
     echo "提交任务: 处理数据 [$start_idx, $end_idx) -> $output_file"
     
     sbatch --job-name="dream_gen_${start_idx}_${end_idx}" \
+            --partition=a100 \
             --output="slurm_logs/dream_gen_${start_idx}_${end_idx}_%j.out" \
             --error="slurm_logs/dream_gen_${start_idx}_${end_idx}_%j.err" \
     << EOF
 #!/bin/bash
-SBATCH --job-name="distill_ar"
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
-#SBATCH --partition=a100
 #SBATCH --mem=30G
 #SBATCH --gpus=1
 #SBATCH --time=01-00:00:00
