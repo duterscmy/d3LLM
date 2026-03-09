534dcc0b705329206d8bbe9651dff62e835d3f52
mingyu cao <m.cao@surrey.ac.uk>
2026-03-09 21:37:55 +0000

    add

--- d3llm/d3llm_DREAM/distill_1_data_prepare/submit_slurm.sh
+++ d3llm/d3llm_DREAM/distill_1_data_prepare/submit_slurm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # 配置参数
-START=30000
+START=0
 # END=122000
-END=60000
-STEP=3000
+END=16000
+STEP=2000
 SCRIPT_PATH="d3llm/d3llm_DREAM/distill_1_data_prepare/d3llm_dream_generate_partly.py"
 
 mkdir -p slurm_logs
